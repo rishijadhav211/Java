8c30d0eb73d9a8d489a358990b989088eadb7650
Rushikesh Jadhav <rishijadhav211@gmail.com>
2023-12-28 23:36:16 +0530

    finished with oop attempt

--- java/Processing_4_Balls/src/main/java/TryProcessing.java
+++ java/Processing_4_Balls/src/main/java/TryProcessing.java
@@ -5,40 +5,60 @@ public class TryProcessing extends PApplet {
     public static final int HEIGHT = 500;
     public static final int WIDTH = 500;
     public static final int DIAMETER = 10;
+    public static int horizontalPosition =1;
+/*
+    shared with all circle so declared in parent class,also can not be declared in nested class
+*/
 
-    int x=1;
+    Circle circle1;
+    Circle circle2;
+    Circle circle3;
+    Circle circle4;
+
+
+
     public static void main(String[] args) {
-        PApplet.main("TryProcessing",args);
+        PApplet.main("TryProcessing", args);
     }
+
     @Override
     public void settings() {
         super.settings();
         size(WIDTH, HEIGHT);
     }
 
+    @Override
+    public void setup() {
+        circle1 = new Circle(1, (float) HEIGHT / 5);
+        circle2 = new Circle(2, ((float) HEIGHT / 5) * 2);
+        circle3 = new Circle(3, ((float) HEIGHT / 5) * 3);
+        circle4 = new Circle(4, ((float) HEIGHT / 5) * 4);
+    }
 
     @Override
     public void draw() {
+        circle1.display();
+        circle2.display();
+        circle3.display();
+        circle4.display();
+        horizontalPosition++;
+    }
 
-        drawCircle1();
-        drawCircle2();
-        drawCircle3();
-        drawCircle4();
-        x++;
-    }
-    
+    class Circle {
+        private final float  horizontalSpeed;
+        private final float verticalPosition;
 
-    private void drawCircle1() {
+        Circle(float HorizontalSpeed, float verticalPosition) {
+            this.horizontalSpeed=HorizontalSpeed;
+            this.verticalPosition = verticalPosition;
+        }
 
-        ellipse(x,HEIGHT/5, DIAMETER,DIAMETER);
-    }
-    private void drawCircle2() {
-        ellipse(x*2,(HEIGHT/5)*2,DIAMETER,DIAMETER);
-    }
-    private void drawCircle3() {
-        ellipse(x*3,(HEIGHT/5)*3,DIAMETER,DIAMETER);
-    }
-    private void drawCircle4() {
-        ellipse(x*4,(HEIGHT/5)*4,DIAMETER,DIAMETER);
+        void display() {
+            drawCircle();
+        }
+
+        private void drawCircle() {
+            ellipse(TryProcessing.horizontalPosition * this.horizontalSpeed, verticalPosition, DIAMETER, DIAMETER);
+        }
     }
 }
