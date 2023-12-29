8c65fa1f659110c011e57c1654022adbb6da5de0
Rushikesh Jadhav <rishijadhav211@gmail.com>
2023-12-29 07:20:28 +0530

    OOP

--- java/Processing_4_Balls/src/main/java/TryProcessing.java
+++ java/Processing_4_Balls/src/main/java/TryProcessing.java
@@ -5,7 +5,8 @@ public class TryProcessing extends PApplet {
     public static final int HEIGHT = 500;
     public static final int WIDTH = 500;
     public static int positionOnXAxis =1;
-/*
+
+    /*
     shared with all circle so declared in parent class,also can not be declared in nested class
 */
 
@@ -13,7 +14,6 @@ public class TryProcessing extends PApplet {
     Circle circle2;
     Circle circle3;
     Circle circle4;
-
 
 
     public static void main(String[] args) {
@@ -28,10 +28,21 @@ public class TryProcessing extends PApplet {
 
     @Override
     public void setup() {
-        circle1 = new Circle(1, (float) HEIGHT / 5);
-        circle2 = new Circle(2, ((float) HEIGHT / 5) * 2);
-        circle3 = new Circle(3, ((float) HEIGHT / 5) * 3);
-        circle4 = new Circle(4, ((float) HEIGHT / 5) * 4);
+        final int SPEED_OF_CIRCLE1 = 1;
+        final float verticalPosition1 = (float) HEIGHT / 5;
+        circle1 = new Circle(SPEED_OF_CIRCLE1, verticalPosition1);
+
+        final int SPEED_OF_CIRCLE2 = 2;
+        final float verticalPosition2 = ((float) HEIGHT / 5) * 2;
+        circle2 = new Circle(SPEED_OF_CIRCLE2, verticalPosition2);
+
+        final int SPEED_OF_CIRCLE3 = 3;
+        final float verticalPosition3 = ((float) HEIGHT / 5) * 3;
+        circle3 = new Circle(SPEED_OF_CIRCLE3, verticalPosition3);
+
+        final int SPEED_OF_CIRCLE4 = 4;
+        final float verticalPosition4 = ((float) HEIGHT / 5) * 4;
+        circle4 = new Circle(SPEED_OF_CIRCLE4, verticalPosition4);
     }
 
     @Override
